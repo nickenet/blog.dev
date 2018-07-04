eff7a729de2f802d3740fe44de730b1b121690f9
nickenet <nicke.ipxp@gmail.com>
2018-07-04 16:07:04 +0300

    create admin dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,11 +1,48 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: nicke
- * Date: 27.06.2018
- * Time: 15:49
- */
+@extends('admin.layouts.app_admin')
 
-@extends('layouts.app')
-
-@section()
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-3 text-sm-center">
+                <div class="jumbotron">
+                    <p><span class="badge badge-primary">Категорий 0</span></p>
+                </div>
+            </div>
+            <div class="col-sm-3 text-sm-center">
+                <div class="jumbotron">
+                    <p><span class="badge badge-primary">Материалов 0</span></p>
+                </div>
+            </div>
+            <div class="col-sm-3 text-sm-center">
+                <div class="jumbotron">
+                    <p><span class="badge badge-primary">Посетителей 0</span></p>
+                </div>
+            </div>
+            <div class="col-sm-3 text-sm-center">
+                <div class="jumbotron">
+                    <p><span class="badge badge-primary">Сегодня 0</span></p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-6">
+                <a class="list-group-item list-group-item-action btn btn-block btn-default text-center" href="#">Создать категорию</a>
+                <a class="list-group-item list-group-item-action" href="#">
+                    <p4 class="list-group-item-heading font-weight-bold ">Категории первая</p4>
+                    <p class="list-group-item-text">
+                        Кол-во материалов
+                    </p>
+                </a>
+            </div>
+            <div class="col-sm-6">
+                <a class="list-group-item list-group-item-action btn btn-block btn-default text-center" href="#">Создать материал</a>
+                <a class="list-group-item list-group-item-action" href="#">
+                    <p4 class="list-group-item-heading font-weight-bold">Материал первый</p4>
+                    <p class="list-group-item-text">
+                        Категория
+                    </p>
+                </a>
+            </div>
+        </div>
+    </div>
+@endsection
